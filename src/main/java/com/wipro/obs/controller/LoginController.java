db287900c891c6f75847ae354476919f4c0a97be
Priyaank <priyaankk@gmail.com>
2022-04-18 12:43:20 +0530

    minor Commit

--- src/main/java/com/wipro/obs/controller/LoginController.java
+++ src/main/java/com/wipro/obs/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.wipro.obs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-//i dont know
 @Controller
 public class LoginController {
 
